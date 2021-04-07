6d784291c25cf5d79e34232974dd07700022f1a9
root <grzegorzchrymecki@php-developer.ie>
2021-04-07 21:47:56 +0100

    x and y

--- assets/components/indexDefault/aside/aside.js
+++ assets/components/indexDefault/aside/aside.js
@@ -1,8 +1,15 @@
 const menuBtn = document.getElementById('aside');
 
 function moveMenu(menu){
-    menu.style.transform = 'translate(1px, 1px)';
-    console.log(menu);
+    const menuTransform = menu.style.transform = 'translate(0px, 0px)';
+    const indexStart = menuTransform.indexOf('(');
+    const indexEnd = menuTransform.indexOf(')');
+    const textBetweenStartEnd = menuTransform.slice(indexStart+1,indexEnd);
+    const splitResult = textBetweenStartEnd.split(',');
+    const [a, b] = splitResult;
+    let x = parseInt(a);
+    let y = parseInt(b);
+
 
 
 }

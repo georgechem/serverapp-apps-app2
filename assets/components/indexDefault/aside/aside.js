f777a677877001836fa539a9ea5dae2414ca4828
root <grzegorzchrymecki@php-developer.ie>
2021-04-08 19:53:21 +0100

    ok

--- assets/components/indexDefault/aside/aside.js
+++ assets/components/indexDefault/aside/aside.js
@@ -1,15 +1,26 @@
 const homeSide = document.getElementsByClassName('menuSide');
 const menuIcons = document.getElementsByClassName('menu__icons');
 
+function sideMenu(icon){
+    console.log(icon);
+}
+
+
 menuIcons.forEach((icon)=>{
     icon.addEventListener('click',(e)=>{
         if(e.target.id === 'home'){
+            sideMenu(e.target.id);
+
             homeSide.forEach((subMenu)=>{
                 if(subMenu.id !== `${e.target.id}Side`){
                     console.log(subMenu.id);
                     subMenu.classList.add('hidden');
                 }else{
                     subMenu.classList.remove('hidden');
+
+                }
+                if(e.target.id === 'home'){
+                    subMenu.style.top = '30px';
                 }
 
             });

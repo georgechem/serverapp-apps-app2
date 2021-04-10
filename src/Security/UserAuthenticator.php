2a88057575d0493c412550f5a06e086f8876e9ba
root <grzegorzchrymecki@php-developer.ie>
2021-04-10 22:57:50 +0100

    ok

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -2,15 +2,45 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class UserAuthenticator extends AbstractGuardAuthenticator
+class UserAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
 {
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    private UrlGeneratorInterface $urlGenerator;
+    private UserPasswordEncoderInterface $passwordEncoder;
+    private CsrfTokenManagerInterface $csrfTokenManager;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, UserPasswordEncoderInterface $passwordEncoder, CsrfTokenManagerInterface $csrfTokenManager)
+    {
+
+        $this->urlGenerator = $urlGenerator;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->csrfTokenManager = $csrfTokenManager;
+        $this->entityManager = $entityManager;
+    }
+
+
     public function supports(Request $request)
     {
 
@@ -22,31 +52,55 @@ class UserAuthenticator extends AbstractGuardAuthenticator
 
     public function getCredentials(Request $request)
     {
-        return $request->request->get('login');
+        $credentials = [
+            'email' => $request->request->get('email'),
+            'password' => $request->request->get('password'),
+            'csrf_token' => $request->request->get('_csrf_token'),
+        ];
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $credentials['email']
+        );
+        return $credentials;
 
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        if(null === $credentials){
-            return null;
+        /*
+        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
+        if (!$this->csrfTokenManager->isTokenValid($token)) {
+            throw new InvalidCsrfTokenException();
+        }*/
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
+
+        if (!$user) {
+            // fail authentication with a custom error
+            throw new CustomUserMessageAuthenticationException('Email could not be found.');
         }
-        return $userProvider->loadUserByUsername($credentials['email']);
+
+        return $user;
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        dd($credentials, $user);
+
+        dd($user);
+        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         // todo
+        dd('bad');
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
         // todo
+        dd('ok');
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
@@ -57,5 +111,16 @@ class UserAuthenticator extends AbstractGuardAuthenticator
     public function supportsRememberMe()
     {
         // todo
+    }
+
+    protected function getLoginUrl()
+    {
+        // TODO: Implement getLoginUrl() method.
+    }
+
+    public function getPassword($credentials): ?string
+    {
+        // TODO: Implement getPassword() method.
+        return $credentials['password'];
     }
 }

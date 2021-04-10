cff28c473e2f988ca4582ad0cc0cca6cf3401ac9
root <grzegorzchrymecki@php-developer.ie>
2021-04-10 08:50:08 +0100

    ok

--- assets/components/indexDefault/aside/aside.js
+++ assets/components/indexDefault/aside/aside.js
@@ -11,21 +11,16 @@ menuSideUls.forEach((menuSideUl)=>{
 
 
 function sideMenu(icon){
-    console.log(icon);
+    //console.log(icon);
     menuSide.forEach((subMenu)=>{
         if(subMenu.id !== `${icon}Side`){
-            console.log(subMenu.id);
+            //console.log(subMenu.id);
             subMenu.classList.add('hidden');
         }else{
             subMenu.classList.toggle('hidden');
 
         }
-        if(icon === 'home'){
-            subMenu.style.top = '30px';
-        }
-        if(icon === 'users'){
-            subMenu.style.top = '97px';
-        }
+
     })
 
 }
